5e0f5ef42e484013590bd68d7331ac29fc8f1641
Steven Dytewski <40008730+Dy2ski@users.noreply.github.com>
2019-04-23 19:56:02 -0700

    Updates

---
+++ precache-manifest.e90a7997a2a88eeb71a0bf2e57f1246b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "f0dfe2e94c7877dbb8d7",
+    "url": "/clicky-game/static/js/main.9dd02b64.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f0dfe2e94c7877dbb8d7",
+    "url": "/clicky-game/static/css/main.3403083e.chunk.css"
+  },
+  {
+    "revision": "bbf9695571541246c364f851d498d657",
+    "url": "/clicky-game/index.html"
+  }
+];
